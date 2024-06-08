9722ea7a44d8a3fa9e5612b646145b2cdec6fcab
Derek Z <51297604+daz005@users.noreply.github.com>
2024-06-07 18:40:23 -0700

    Create P8.java

---
+++ hw2/P8.java
@@ -0,0 +1,13 @@
+package com.jhu.hw2
+
+public class P8 extends Aircraft {
+  private int numberEngines;
+
+  public int getNumberEngines() {
+    return numberEngines;
+  }
+
+  public void setNumberEngines(int numberEngines) {
+    this.numberEngines = numberEngines;
+  }
+}
