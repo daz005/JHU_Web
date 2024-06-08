f44cd33f0d6bd6af24f559cfb7ae956e0b160119
Derek Z <51297604+daz005@users.noreply.github.com>
2024-06-07 19:01:16 -0700

    Create Test.java

---
+++ hw2/Test.java
@@ -0,0 +1,66 @@
+package com.jhu.hw2
+
+public class Test {
+  public static void main(String[] args) {
+    Destroyer destroyer1 = new Destroyer();
+    destroyer1.setName("Destroyer 1");
+    destroyer1.setNumberMissile(10);
+
+    Destroyer destroyer2 = new Destroyer();
+    destroyer1.setName("Destroyer 2");
+    destroyer1.setNumberMissile(15);
+
+    Submarine submarine1 = new Submarine();
+    submarine1.setName("Submarine 1");
+    submarine1.setNumberTorpedos(8);
+
+    Submarine submarine2 = new Submarine();
+    submarine1.setName("Submarine 2");
+    submarine1.setNumberTorpedos("Foo");
+
+    P8 p8_1 = new P8();
+    p8_1.setName("P8 1");
+    p8_1.setNumberEngines(4);
+
+    P8 p8_2 = new P8();
+    p8_2.setName("P8 2");
+    p8_2.setNumberEngines(4);
+
+    List<Destroyer> destroyerList = new ArrayList<>();
+    destroyerList.add(destroyer1);
+    destroyerList.add(destroyer2);
+
+    List<Submarine> submarineList = new ArrayList<>();
+    submarineList.add(submarine1);
+    submarineList.add(submarine2);
+
+    List<Ship> shipList = new ArrayList<>();
+    shipList.add(destroyer1);
+    shipList.add(destroyer2);
+    shipList.add(submarine1);
+    shipList.add(submarine2);
+
+    List<Contact> contactList = new Arraylist<>();
+    contactList.add(destroyer1);
+    contactList.add(destroyer2);
+    contactList.add(submarine1);
+    contactList.add(submarine2);
+    contactList.add(p8_1);
+    contactList.add(p8_2);
+
+    System.out.println("Destroyers:");
+    for (Destroyer d : destroyerList) {
+      System.out.println(d.getName() + " - Missiles: " + d.getNumberMissile());
+    }
+
+    System.out.println("Submarines:");
+    for (Submarine s : submarineList) {
+      System.out.println(s.getName() + " - Torpedos: " + s.getNumberTorpedos());
+    }
+
+    System.out.println("P8 Aircrafts:");
+    System.out.println(p8_1.getName() + " - Engines: " + p8_1.getNumberEngines());
+    System.out.println(p8_2.getName() + " - Engines: " + p8_2.getNumberEngines());
+    }
+    
+    
