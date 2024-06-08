072646e4cc22591638376c6d7bc3c69bfe115ea7
Derek Z <51297604+daz005@users.noreply.github.com>
2024-06-07 17:55:07 -0700

    Update Contact.java

--- hw2/Contact.java
+++ hw2/Contact.java
@@ -1,1 +1,15 @@
+package com.jhu_hw2.contact;
 
+public interface Contact {
+
+  int getLength();
+  void setLength(int length);
+  int getSpeed();
+  void setSpeed(int speed);
+  void setSpeed(String speed);
+  String getName();
+  void setName(String name);
+  String getType();
+  void setType(String type);
+
+}
