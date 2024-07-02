255a7e5f6d4c0e5c7d275b80fbc7aa37eb337af9
Derek Z <51297604+daz005@users.noreply.github.com>
2024-07-02 13:18:07 +0800

    Update script.js

--- hw7/MyServletApp/webapp/script.js
+++ hw7/MyServletApp/webapp/script.js
@@ -1,1 +1,19 @@
 
+document.getElementById('dataForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+
+    fetch('http://localhost:8080/MyServletApp/MyServlet', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name: name })
+    })
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('response').innerText = data;
+    })
+    .catch(error => console.error('Error:', error));
+});
