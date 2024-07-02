b6d03633fa7520636662df2b7151da215f702438
Derek Z <51297604+daz005@users.noreply.github.com>
2024-07-02 13:18:36 +0800

    Update MyServlet.java

--- hw7/MyServletApp/src/MyServlet.java
+++ hw7/MyServletApp/src/MyServlet.java
@@ -1,1 +1,31 @@
+package com.example;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import org.json.JSONObject;
+
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        StringBuilder buffer = new StringBuilder();
+        String line;
+        while ((line = request.getReader().readLine()) != null) {
+            buffer.append(line);
+        }
+        JSONObject jsonObject = new JSONObject(buffer.toString());
+        String name = jsonObject.getString("name");
+
+        response.setContentType("text/plain");
+        PrintWriter out = response.getWriter();
+        out.println("Hello, " + name + "!");
+        out.close();
+    }
+}
 
